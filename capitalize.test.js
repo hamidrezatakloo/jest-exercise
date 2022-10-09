9a772ec9f2ef7399ac5e06ef71b0ae05a56c1b26
hamidreza <takloohamidreza@gmail.com>
2022-10-09 19:55:16 +0330

    test: add test for 'ZORO' to 'Zoro'

--- capitalize.test.js
+++ capitalize.test.js
@@ -3,3 +3,7 @@ import capitalize from "./capitalize";
 test("luffy capitalize to Luffy", () => {
   expect(capitalize("luffy")).toBe("Luffy");
 });
+
+test("ZORO capitalize to Zoro", () => {
+  expect(capitalize("ZORO").toBe("Zoro"));
+});
