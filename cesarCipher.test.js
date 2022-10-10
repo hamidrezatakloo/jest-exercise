d78ce3d35c7a177c892c6fb72d6fe1413ac1eef7
hamidreza <takloohamidreza@gmail.com>
2022-10-10 10:18:08 +0330

    test: create test for uppercase plaintext

--- cesarCipher.test.js
+++ cesarCipher.test.js
@@ -5,3 +5,7 @@ test("shift with key 10 ", () => {
 test("plaintext with white space ", () => {
   expect(cesarCipher("attack on titan", 4)).toEqual("exxego sr xmxer");
 });
+
+test("UpperCase plaintext ", () => {
+  expect(cesarCipher("MISTSURY", 6)).toEqual("qmwxwyvc");
+});
