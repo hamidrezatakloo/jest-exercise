a7f241285d6dbb2020a6d312e9f5555a9286313e
hamidreza <takloohamidreza@gmail.com>
2022-10-10 11:03:23 +0330

    feat: create cipher text with shift in each char

--- cesarCipher.js
+++ cesarCipher.js
@@ -4,4 +4,15 @@ const cesarCipher = (plaintext, key) => {
     asciiCode = (asciiCode + key) % 26;
     return String.fromCharCode(asciiCode);
   };
+  let cipher = "";
+  let textList = plaintext.split("");
+  textList.forEach((c) => {
+    const asci = c.charCodeAt(0);
+
+    if (asci > 96 && asci < 123) cipher += shift(c);
+    else if (asci > 65 && asci < 90) cipher += shift(c.toLowerCase());
+    else cipher += c;
+  });
+
+  return cipher;
 };
