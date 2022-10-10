95900c99506a8cbd3430acd730076c303f5c78c7
hamidreza <takloohamidreza@gmail.com>
2022-10-10 10:15:35 +0330

    test: create test for plaintext with whitespace

--- cesarCipher.test.js
+++ cesarCipher.test.js
@@ -2,3 +2,6 @@ test("shift with key 10 ", () => {
   expect(cesarCipher("lol", 10)).toEqual("vyv");
 });
 
+test("plaintext with white space ", () => {
+  expect(cesarCipher("attack on titan", 4)).toEqual("exxego sr xmxer");
+});
