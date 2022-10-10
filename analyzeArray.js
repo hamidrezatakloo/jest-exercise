cfd65e7e0850aba3e85dcc0d631b4f431d1c6559
hamidreza <takloohamidreza@gmail.com>
2022-10-10 13:33:44 +0330

    fix: round average after calculated

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,10 +1,11 @@
 const analyzeArray = (array) => {
   const length = array.length;
   if (length == 0) return "error";
-  const average =
+  let average =
     array.reduce((sum, number) => {
       return sum + number;
     }) / length;
+  average = Math.floor(average);
 
   const min = Math.min(...array);
   const max = Math.max(...array);
