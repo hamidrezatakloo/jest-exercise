7270799193e7e8c9a0fcfc8599c5405112c90c47
hamidreza <takloohamidreza@gmail.com>
2022-10-10 10:23:44 +0330

    test: write better text for   tests

--- cesarCipher.test.js
+++ cesarCipher.test.js
@@ -1,12 +1,12 @@
-test("shift with key 10 ", () => {
+test("shift with key 10", () => {
   expect(cesarCipher("lol", 10)).toEqual("vyv");
 });
 
-test("plaintext with white space ", () => {
+test("plaintext with white space return cipher with white space", () => {
   expect(cesarCipher("attack on titan", 4)).toEqual("exxego sr xmxer");
 });
 
-test("UpperCase plaintext ", () => {
+test("UpperCase plaintext return lowerCase cipher", () => {
   expect(cesarCipher("MISTSURY", 6)).toEqual("qmwxwyvc");
 });
 
